0c6081123548aef20085f6d147ec68fe1a6e30a7
Balazs Makrai <b.makrai@gmail.com>
2022-12-02 14:26:35 +0100

    add html and style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import { defineNuxtConfig } from "nuxt3";
 
-})
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    WEATHER_APP_SECRET: process.env.WEATHER_APP_SECRET,
+  },
+  privateRuntimeConfig: {
+    HELLO: "world in the server not the client",
+  },
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
