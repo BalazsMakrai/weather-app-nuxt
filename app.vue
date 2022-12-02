d34a464e7f2c11d7ab596fde995cfc9ca84ce0b4
Balazs Makrai <b.makrai@gmail.com>
2022-12-02 15:04:52 +0100

    add icons

--- app.vue
+++ app.vue
@@ -5,23 +5,34 @@
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
-          <h1 class="text-7xl text-white">Toronto</h1>
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
           <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
-          <img class="w-56 icon" />
+          <img :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`" class="w-56 icon" />
         </div>
         <div>
-          <p class="text-9xl text-white font-extralight">53°</p>
+          <p class="text-9xl text-white font-extralight">{{city.main.temp}} °C</p>
         </div>
       </div>
       <div class="mt-20">
-        <input type="text" class="w-1/2 h10" placeholder="Search a city..."/>
+        <input type="text" class="w-1/2 h10" placeholder="Search a city..." />
         <button class="bg-sky-400 w-20 text-white h-10">Search</button>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+//https://api.openweathermap.org/data/2.5/weather?q=Debrecen&appid=cef17c0d00e16108b03e2dd6314f6b30
+
+const { data: city, error } = useFetch("https://api.openweathermap.org/data/2.5/weather?q=Debrecen&units=metric&appid=cef17c0d00e16108b03e2dd6314f6b30");
+</script>
 <style scoped>
 .overlay {
   background-color: rgba(0, 0, 0, 0.5);
+}
+
+.icon {
+  margin-left: -2.75rem;
+  margin-top: -2.75rem;
 }
 </style>
